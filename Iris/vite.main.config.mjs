313a51da53757d336c96c1d3992b7a9a476a135a
Hyperiya <hyperiya.vcz@outlook.com>
2025-08-12 00:00:12 +0200

    PLEASE

--- Iris/vite.main.config.mjs
+++ Iris/vite.main.config.mjs
@@ -1,23 +1,36 @@
-import { defineConfig } from 'vite';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   build: {
-    outDir: '.vite/build',
+    outDir: ".vite/build",
     target: "es2020",
     lib: {
-      entry: path.resolve(__dirname, 'src/main.ts'),
-      formats: ['es'],
-      fileName: () => 'main.js'
+      entry: path.resolve(__dirname, "src/main.ts"),
+      formats: ["es"],
+      fileName: () => "main.js",
     },
     rollupOptions: {
-      external: ['electron'],
+      external: [
+        "electron",
+        "fs",
+        "path",
+        "util",
+        "child_process",
+        "url",
+        "node:fs",
+        "node:util",
+        "node:path",
+        "node:child_process",
+        "node:url"
+      ],
+
       output: {
-        format: 'es',
-      }
+        format: "es",
+      },
     },
     emptyOutDir: false,
     minify: false,
